3ff8e0862f03bec28f8f3a17a4d9e56c9d0dfbb9
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 21:36:54 +0300

    add prototype for cost of order based on size => small, medium, large.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,30 @@ function Pizza(toppings, size) {
   this.size = size;
 }
 
+Pizza.prototype.costBasedOnSize = function() {
+  if (this.size === "small") {
+    return 4;
+  } else if (this.size === "medium") {
+    return 5;
+  } else if (this.size === "large") {
+    return 6;
+  } else {
+    
+  }
+}
+
+Pizza.prototype.costbasedOnTopping = function() {
+  if (this.toppings.length != 0) {
+    let toppingCost = this.toppings.length * 4;
+    return toppingCost;
+  } else {
+
+  }
+}
+
+Pizza.prototype.tabulate = function(tCost, sCost) {
+  let totalCost = tCost + sCost;
+  $("displayTotalCost").html(totalCost)
+}
+
 
