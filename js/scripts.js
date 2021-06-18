5650b84534b5c3ec8a3bfe0244fa8b748e821f02
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 20:45:47 +0300

    add pizzaparlor constructor holding all pizze flavors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 function PizzaParlor() {
-  this.pizza = pizza;
+  this.pizze = {};
+  this.toppings = toppings;
 }
 
-function pizza(cheese, pepperoni, mushrooms, small, large) {
+PizzaParlor.prototype.orderPizza = function(pizza) {
+  this.pizze[pizza.toppings] = pizza;
+  if  
+}
+
+function Toppings(cheese, pepperoni, mushrooms) {
   this.cheese = cheese;
   this.pepperoni = pepperoni;
   this.mushrooms = mushrooms;
+}
+
+function Size(small, medium, large) {
   this.small = small;
+  this.medium = medium;
   this.large = large;
 }
+function Pizza(Toppings, Size) {
+  this.Toppings = Toppings;
+  this.Size = Size;  
+}
+
+Pizza.prototype.order = function() {
+  return this.Toppings + " " + this.Size;
+}
+
