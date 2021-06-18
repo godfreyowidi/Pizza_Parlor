2f81c5a2b431b09510a79dcac5a921305db78019
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 21:08:24 +0300

    add parameters of size and topping to pizza constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,6 @@
-function PizzaParlor() {
-  this.pizze = {};
+function Pizza(toppings, size) {
   this.toppings = toppings;
+  this.size = size;
 }
 
-PizzaParlor.prototype.orderPizza = function(pizza) {
-  this.pizze[pizza.toppings] = pizza;
-  if  
-}
-
-function Toppings(cheese, pepperoni, mushrooms) {
-  this.cheese = cheese;
-  this.pepperoni = pepperoni;
-  this.mushrooms = mushrooms;
-}
-
-function Size(small, medium, large) {
-  this.small = small;
-  this.medium = medium;
-  this.large = large;
-}
-function Pizza(Toppings, Size) {
-  this.Toppings = Toppings;
-  this.Size = Size;  
-}
-
-Pizza.prototype.order = function() {
-  return this.Toppings + " " + this.Size;
-}
 
