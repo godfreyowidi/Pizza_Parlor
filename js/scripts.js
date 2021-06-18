3bde978870510119965c79126d6e031b71bd01b7
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 23:53:45 +0300

    add custom css to the bage +bg image.

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ Pizza.prototype.costbasedOnTopping = function() {
 
 Pizza.prototype.tabulate = function(tCost, sCost) {
   let totalCost = tCost + sCost;
-  $("showTotalCost").html(totalCost)
+  $("#showTotalCost").html(totalCost)
 }
 
 //UI
@@ -35,16 +35,20 @@ let pizza;
 $(document).ready(function() {
   $("#formField").submit(function(event) {
     event.preventDefault();
-    $("invoice").show();
+    $("#invoice").show();
     let inputtedName = $("#name").val();
     let inputtedToppings = parseInt($("toppings").val());
     let toppingTray = [];
     $("input:checkbox[name=toppingCheck]:checked").each(function() {
       let inputtedToppings = $(this).val();
       toppingTray.push(inputtedToppings);
-      $("displayToppings").text(inputtedToppings + ",");
+      $("#displayToppings").text(inputtedToppings + ",");
     });
-    let pizza = new Pizza(toppingTray, inputtedSize);
+    let inputtedCheckedSize =$("input:radio[name=sizePizza]:checked").val();
+    $("#displayName").text(inputtedName);
+    $("#displaySize").text(inputtedCheckedSize);
+
+    pizza = new Pizza(toppingTray, inputtedSize);
     let costbasedOnToppings = pizza.costbasedOnTopping();
     let costBasedOnSizes = pizza.costBasedOnSize();
     pizza.tabulate(costbasedOnToppings, costBasedOnSizes);
