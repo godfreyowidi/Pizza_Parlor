99a79e99bfeab962191a44d63e2147e20cc04892
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 22:23:56 +0300

    WIP: add user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business logic
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
@@ -24,6 +25,30 @@ Pizza.prototype.costbasedOnTopping = function() {
   }
 }
 
+Pizza.prototype.tabulate = function(tCost, sCost) {
+  let totalCost = tCost + sCost;
+  $("showTotalCost").html(totalCost)
+}
 
+//UI
+let pizza;
+$(document).ready(function() {
+  $("#formField").submit(function(event) {
+    event.preventDefault();
+    $("invoice").show();
+    let inputtedName = $("#name").val();
+    let inputtedToppingsQuantity = parseInt($("toppings").val());
+    let toppingTray = [];
+    $("input:checkbox[name=toppingCheck]:checked").each(function() {
+      let inputtedToppings = $(this).val();
+      toppingTray.push(inputtedToppings);
+      $("displayToppings").text(inputtedToppings + ",");
+    });
+    let pizza = new Pizza(toppingTray, inputtedSize);
+    let costbasedOnToppings = pizza.costbasedOnTopping();
+    let costBasedOnSizes = pizza.costBasedOnSize();
+    pizza.tabulate(costbasedOnToppings, costBasedOnSizes);
+  });
+});
 
 
