06a5c705ff547d3a62c0abeae477720e68ec9af5
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-18 19:48:14 +0300

    add pizza constructor with 5 properties.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function PizzaParlor() {
+  this.pizza = pizza;
+}
+
+function pizza(cheese, pepperoni, mushrooms, small, large) {
+  this.cheese = cheese;
+  this.pepperoni = pepperoni;
+  this.mushrooms = mushrooms;
+  this.small = small;
+  this.large = large;
+}
